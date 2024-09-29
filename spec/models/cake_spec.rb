8ef0d8518914beb93de35bde348887c49c071e0f
buihung-wogi <bui.hung@wogi.biz>
2024-09-29 09:05:03 +0700

    update review dog

--- spec/models/cake_spec.rb
+++ spec/models/cake_spec.rb
@@ -4,4 +4,8 @@ describe Cake do
   describe '#sweet?' do
     it { expect(subject.sweet?).to be true }
   end
+
+  describe '#for_children?' do
+    it { expect(subject.for_children?).to be true }
+  end
 end
