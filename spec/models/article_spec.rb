5f3c732bf8ae07a9188d878971dc52bbf8addb4a
buihung-wogi <bui.hung@wogi.biz>
2024-09-29 11:56:20 +0700

    update review dog

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  describe '#is_odd?' do
-    it { expect(User.new.is_odd?).to be_in([ true, false ]) }
+RSpec.describe Article, type: :model do
+  describe '#even?' do
+    it { expect(Article.new.even?).to be_in([ true, false ]) }
   end
 
-  describe '#is_even?' do
-    it { expect(User.new.is_even?).to be_in([ true, false ]) }
+  describe '#odd?' do
+    it { expect(Article.new.odd?).to be_in([ true, false ]) }
   end
 end
